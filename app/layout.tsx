9f1925812c24dae28fbd2aaac72332e1c25b4c0e
Nico Anastasio <nico@attractiontickets.com>
2023-08-06 10:49:49 +0100

    added nav and hero

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,13 @@
-// import './globals.css'
-import 'bootstrap/dist/css/bootstrap.css'
-import "app/css/customcss.css"
-import "@fortawesome/fontawesome-svg-core/styles.css";
+import './globals.css'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import "app/css/customcss.css"
+// import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false;
 
-import { Comfortaa } from 'next/font/google'
-import Header from './components/Header'
+import { Comfortaa } from 'next/font/google';
+import Header from './components/Header';
+import Hero from './components/Hero';
 
 
 const comfortaa = Comfortaa({ subsets: ['latin'] })
@@ -23,8 +24,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={ `container-fluid text-center ${comfortaa.className}` }>
+      <body className={ `container-fluid ${comfortaa.className}` }>
         <Header/>
+        <Hero/>
         {children}
 
         {/* <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous" /> */}
